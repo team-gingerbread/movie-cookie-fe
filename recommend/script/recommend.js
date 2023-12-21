6236694019fdfb37f8d54df4ae6742509db36fc9
Haesun-Pyeon <sme1357@naver.com>
2023-12-21 11:54:25 +0900

    ✨ ADD: 공통 부분 js추가, base.css공통적용

--- recommend/script/recommend.js
+++ recommend/script/recommend.js
@@ -1,5 +1,10 @@
 // recommend/script/recommend.js - 추천 입력 JS
+import { backend } from "/script/url.js";
+import { getToken } from "/script/token.js";
 console.log("recommend.js 연결");
+
+const token = await getToken();
+console.log(backend, token);
 
 const $submit = document.querySelector('button[type="submit"]');
 $submit.addEventListener("click", async function (e) {
@@ -12,7 +17,7 @@ $submit.addEventListener("click", async function (e) {
         input_period: document.querySelector("#period").value,
     };
 
-    const response = await fetch("http://52.79.54.190/recommend/generate/", {
+    const response = await fetch(`${backend}recommend/generate/`, {
         headers: {
             "Content-Type": "application/json",
         },
@@ -28,6 +33,7 @@ $submit.addEventListener("click", async function (e) {
     }
 
     // 성공 시 상세보기 창으로 넘어감
+    // 성공 시 로그인여부 확인해서 로그인하면 저장요청 또보내고 로그인안했으면 로컬스토리지에 저장하고 상세보기 창으로 넘어가기? 로컬에 저장할때는 구분값 id같은걸 키로 줘서 그걸로 받아오면 될듯 - 근데이제 id는 로컬스토리지 길이로 차례차례 1부터 추가???
     const res = await response.json();
     console.log(res);
     // window.location.replace(`/recommend/detail/?id=${res.id}`);
---
+++ script/token.js
@@ -0,0 +1,42 @@
+import { backend } from "./url.js";
+
+export async function getToken() {
+    // 로컬스토리지에 저장된 토큰 가져옴
+    const accessToken = localStorage.getItem("access");
+    if (!accessToken) {
+        return null; // 없을 시 null 리턴
+    }
+
+    const payload = JSON.parse(atob(accessToken.split(".")[1]));
+    const exp = payload.exp;
+    const now = Date.now() / 1000;
+
+    if (now >= exp) {
+        // 유효기간이 지났을 경우 재발급
+        return await refreshToken();
+    }
+
+    return accessToken;
+}
+
+async function refreshToken() {
+    // refresh 토큰을 이용하여 재발급
+    const refresh = localStorage.getItem("refresh");
+    const response = await fetch(`${backend}accounts/token/refresh/`, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+        method: "POST",
+        body: JSON.stringify({ refresh: refresh }),
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+        return null; // 재발급 실패 시 null 리턴
+    }
+
+    // 성공 시 로컬스토리지에 access token 새로 저장 후 값 리턴
+    localStorage.setItem("access", data.access);
+    return data.access;
+}

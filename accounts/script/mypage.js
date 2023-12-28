a9d873d1ea390b559e218954912a440bd911a835
bg.lee <qudrhkslee@gmail.com>
2023-12-28 16:55:57 +0900

    🐛BUG: 프로필 영화포스터 조회 버그 수정

--- accounts/script/mypage.js
+++ accounts/script/mypage.js
@@ -13,7 +13,6 @@ document.addEventListener("DOMContentLoaded", async function () {
         `;
     }
 
-    // 프로필 이미지 클릭 시 파일 업로드 창 표시
     document.querySelector(".userimg").addEventListener("click", function () {
         const fileInput = document.createElement("input");
         fileInput.type = "file";
@@ -25,6 +24,38 @@ document.addEventListener("DOMContentLoaded", async function () {
                 updateProfileImage(file);
             }
         });
+
+        async function updateProfileImage(file) {
+            const accessToken = await getToken();
+
+            if (!accessToken) {
+                alert("로그인이 필요합니다.");
+                window.location.href = "/accounts/login/";
+                return;
+            }
+
+            const formData = new FormData();
+            formData.append("profile_picture", file);
+
+            try {
+                const response = await fetch(`${backend}accounts/api/user-profile/${getUserIdFromToken(accessToken)}/`, {
+                    method: "PATCH",
+                    headers: {
+                        Authorization: "Bearer " + accessToken,
+                    },
+                    body: formData,
+                });
+
+                if (!response.ok) {
+                    throw new Error("프로필 이미지를 업데이트하는 데 실패했습니다.");
+                }
+
+                const data = await response.json();
+                updateProfileSection(data);
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        }
 
         fileInput.click();
     });
@@ -38,8 +69,6 @@ document.addEventListener("DOMContentLoaded", async function () {
     document.getElementById("profile-edit").addEventListener("click", function () {
         window.location.href = "/accounts/mypage-edit/";
     });
-
-    displayUserMovies(accessToken);
 });
 
 // 토큰에서 사용자 ID 추출하는 함수
@@ -55,7 +84,6 @@ function getUserIdFromToken(token) {
     }
 }
 
-// 프로필 정보 가져오기
 async function fetchUserProfile() {
     const accessToken = await getToken();
 
@@ -65,44 +93,40 @@ async function fetchUserProfile() {
         return;
     }
 
-    fetch(`${backend}accounts/api/user-profile/${getUserIdFromToken(accessToken)}`, {
-        headers: {
-            Authorization: "Bearer " + accessToken,
-        },
-    })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("프로필 정보를 불러오는 데 실패했습니다.");
-            }
-            return response.json();
-        })
-        .then((data) => {
-            console.log("서버 응답 데이터:", data);
-            updateProfileSection(data);
-        })
-        .catch((error) => {
-            console.error("Error:", error);
+    try {
+        const response = await fetch(`${backend}accounts/api/user-profile/${getUserIdFromToken(accessToken)}`, {
+            headers: {
+                Authorization: "Bearer " + accessToken,
+            },
         });
+
+        if (!response.ok) {
+            throw new Error("프로필 정보를 불러오는 데 실패했습니다.");
+        }
+
+        const data = await response.json();
+        console.log("서버 응답 데이터:", data);
+        updateProfileSection(data);
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
 
-// 프로필 섹션 업데이트
 function updateProfileSection(userData) {
+    const userimgElement = document.querySelector(".userimg");
+    userimgElement.style.backgroundImage = `url(${userData.profile_picture || "/img/profile_basic.png"})`;
+    userimgElement.style.backgroundSize = "cover";
+
     document.querySelector(".username").textContent = userData.username;
     document.querySelector(".nickname").textContent = userData.nickname;
     document.querySelector(".genre").textContent = userData.genre;
     document.querySelector(".bio").textContent = userData.bio || "소개가 없습니다.";
-
-    if (userData.profile_picture) {
-        document.querySelector(".userimg").style.backgroundImage = `url(${userData.profile_picture})`;
-        document.querySelector(".userimg").style.backgroundSize = "cover";
-    } else {
-        document.querySelector(".userimg").style.backgroundImage = `url(/img/profile_basic.png)`;
-        document.querySelector(".userimg").style.backgroundSize = "cover";
-    }
 }
 
-// 사용자가 좋아요한 영화, 본 영화, 볼 영화 가져와서 표시
-async function displayUserMovies(accessToken) {
+document.addEventListener("DOMContentLoaded", async function () {
+    const accessToken = await getToken();
+
+    // 사용자가 좋아요한 영화 가져오기
     const likedMoviesResponse = await fetch(`${backend}accounts/api/liked-movies/`, {
         method: "GET",
         headers: {
@@ -113,10 +137,16 @@ async function displayUserMovies(accessToken) {
     if (likedMoviesResponse.ok) {
         const likedMoviesData = await likedMoviesResponse.json();
         console.log("좋아요한 영화 데이터:", likedMoviesData);
-        const likedMoviesWithPosters = await fetchPosters(likedMoviesData);
+
+        // 좋아요한 영화의 ID 배열 추출
+        const likedMovieIds = likedMoviesData.map((entry) => entry.movie);
+
+        // 각 영화의 포스터 정보 가져오기
+        const likedMoviesWithPosters = await fetchPosters(likedMovieIds);
         displayMoviesInSection("liked-movies-container", likedMoviesWithPosters);
     }
 
+    // 사용자가 본 영화 가져오기
     const watchedMoviesResponse = await fetch(`${backend}accounts/api/watched-movies/`, {
         method: "GET",
         headers: {
@@ -126,10 +156,16 @@ async function displayUserMovies(accessToken) {
 
     if (watchedMoviesResponse.ok) {
         const watchedMoviesData = await watchedMoviesResponse.json();
-        const watchedMoviesWithPosters = await fetchPosters(watchedMoviesData);
+
+        // 본 영화의 ID 배열 추출
+        const watchedMovieIds = watchedMoviesData.map((entry) => entry.movie);
+
+        // 각 영화의 포스터 정보 가져오기
+        const watchedMoviesWithPosters = await fetchPosters(watchedMovieIds);
         displayMoviesInSection("watched-movies-container", watchedMoviesWithPosters);
     }
 
+    // 사용자가 볼 영화 가져오기
     const watchlistMoviesResponse = await fetch(`${backend}accounts/api/watchlist-movies/`, {
         method: "GET",
         headers: {
@@ -139,19 +175,24 @@ async function displayUserMovies(accessToken) {
 
     if (watchlistMoviesResponse.ok) {
         const watchlistMoviesData = await watchlistMoviesResponse.json();
-        const watchlistMoviesWithPosters = await fetchPosters(watchlistMoviesData);
+
+        // 볼 영화의 ID 배열 추출
+        const watchlistMovieIds = watchlistMoviesData.map((entry) => entry.movie);
+
+        // 각 영화의 포스터 정보 가져오기
+        const watchlistMoviesWithPosters = await fetchPosters(watchlistMovieIds);
         displayMoviesInSection("watchlist-movies-container", watchlistMoviesWithPosters);
     }
-}
+});
 
 // 각 영화의 포스터 URL을 가져오는 함수
-async function fetchPosters(moviesData) {
+async function fetchPosters(movieIds) {
     const accessToken = await getToken();
     const moviesWithPosters = [];
 
-    for (const movie of moviesData) {
+    for (const movieId of movieIds) {
         // 각각의 영화에 대한 추가 정보 가져오기
-        const movieInfoResponse = await fetch(`${backend}movieinfo/detail/${movie.id}/`, {
+        const movieInfoResponse = await fetch(`${backend}movieinfo/detail/${movieId}/`, {
             method: "GET",
             headers: {
                 Authorization: "Bearer " + accessToken,
@@ -160,10 +201,16 @@ async function fetchPosters(moviesData) {
 
         if (movieInfoResponse.ok) {
             const movieInfoData = await movieInfoResponse.json();
+            console.log("영화 정보 데이터:", movieInfoData); // 디버깅을 위한 로그 추가
+
+            // 포스터 URL을 가져오는 함수를 통해 포스터 URL을 얻습니다.
+            const posterUrl = getFirstPosterUrl(movieInfoData);
+
             // 포스터 URL을 추가한 새로운 객체 생성
             const movieWithPoster = {
-                ...movie,
-                posterUrl: getFirstPosterUrl(movieInfoData),
+                id: movieId, // 영화 ID 추가
+                movie: movieInfoData, // 영화 정보 추가
+                posterUrl: posterUrl, // 포스터 URL 추가
             };
             moviesWithPosters.push(movieWithPoster);
         }
@@ -181,16 +228,17 @@ function displayMoviesInSection(sectionId, moviesData) {
         const movieElement = document.createElement("div");
         movieElement.classList.add("movie");
 
-        // 포스터 이미지 추가
         const posterElement = document.createElement("img");
-        posterElement.src = movie.posterUrl;
-        posterElement.alt = movie.title;
+        posterElement.src = getFirstPosterUrl(movie.movie) || "/img/default.jpg"; // 수정: movie.movie로 접근
+
+        console.log("포스터 URL:", getFirstPosterUrl(movie)); // 디버깅을 위한 로그 추가
+        posterElement.alt = movie.movie.title; // 여기도 수정
         posterElement.classList.add("movie-poster");
         movieElement.appendChild(posterElement);
 
         // 영화 제목 추가
         const titleElement = document.createElement("h2");
-        titleElement.textContent = movie.title;
+        titleElement.textContent = movie.movie.title; // 여기도 수정
         movieElement.appendChild(titleElement);
 
         movieGrid.appendChild(movieElement);
@@ -199,8 +247,8 @@ function displayMoviesInSection(sectionId, moviesData) {
     sectionContainer.appendChild(movieGrid);
 }
 
-function getFirstPosterUrl(movieInfo) {
-    const posters = movieInfo.posters || [];
+function getFirstPosterUrl(movie) {
+    const posters = movie.posters || []; // 수정: movie.posters로 접근
     const firstPoster = posters[0];
     const posterUrl = firstPoster ? firstPoster.url : "/img/default.jpg";
     console.log("포스터 URL:", posterUrl);
